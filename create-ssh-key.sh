eeaffb1c8dfe36d3e5595a0e5bff388483804e28
Mateusz Jędrzejewski <mateusz.jedrzejewski@yahoo.pl>
2021-08-02 21:25:43 +0200

    change email to keyname as a comment

--- create-ssh-key.sh
+++ create-ssh-key.sh
@@ -9,6 +9,6 @@ then
     echo "==> Usage: ./create-ssh-key.sh KEY_NAME"
     exit 1
 else
-    ssh-keygen -qa 100 -t ed25519 -C "devops@nethermind.io" -f .workspace/$KEY_NAME -q -N ""
+    ssh-keygen -qa 100 -t ed25519 -C "$KEY_NAME" -f .workspace/$KEY_NAME -q -N ""
     ssh-add .workspace/$KEY_NAME
 fi
