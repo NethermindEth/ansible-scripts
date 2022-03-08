90c6913426e7a3a41595fdbbdbf6f74e6932757e
Nick Kjolsing <49295025+nickkjolsing@users.noreply.github.com>
2022-03-08 09:17:34 -0600

    feat: added apt-get for installing Nethermind (#5)
    * Added Nethermind install via apt. Changed dotnet version to 6.0 in dependency.
    
    * Update inventory.yml
    
    * Update .env
    
    * changed directory names. added roles for apt.
    
    - changed directory names to distinguish building from source vs apt
    - added role for installing via apt
    - added role for starting service via apt
    - changed dependency to .NET 6.0 from 5.0
    
    * feat: Changed shell script to use apt playbooks. Removed testing inventory.
    
    * Default systemd service to use apt to install Nethermind. Added apt support.

--- setup-nethermind.sh
+++ setup-nethermind.sh
@@ -9,7 +9,6 @@ KEY_PATH=$4
 CHAIN_NAME=$5
 BRANCH_NAME=${6:-master}
 
-
 help() {
   echo "
 Setup Nethermind Node with Systemd Service
@@ -44,11 +43,10 @@ else
     
     if [ "$SERVICE_TYPE" == "systemd" ]; then
       echo "==> Setting up Nethermind environment"
-      ansible-playbook -i "$PUBLIC_IP," --private-key $KEY_PATH --extra-vars "ansible_user=nethermind nethermind_branch=$BRANCH_NAME" playbooks/setup-nethermind.yml
-
+      ansible-playbook -i "$PUBLIC_IP," --private-key $KEY_PATH --extra-vars "ansible_user=nethermind nethermind_branch=$BRANCH_NAME" playbooks/setup-nethermind-apt.yml
       echo "==> Setting up Nethermind systemd service and starting it"
       sed -i '' "s/NETHERMIND_CONFIG=.*/NETHERMIND_CONFIG=$CHAIN_NAME/" roles/nethermind-service/files/.env
-      ansible-playbook -i "$PUBLIC_IP," --private-key $KEY_PATH --extra-vars "ansible_user=nethermind" playbooks/start-nethermind.yml
+      ansible-playbook -i "$PUBLIC_IP," --private-key $KEY_PATH --extra-vars "ansible_user=nethermind" playbooks/start-nethermind-apt.yml
       sed -i '' "s/NETHERMIND_CONFIG=.*/NETHERMIND_CONFIG=mainnet_pruned/" roles/nethermind-service/files/.env
     else
       echo "==> Setting up Nethermind docker-compose stack and starting it"
@@ -57,14 +55,3 @@ else
       sed -i '' "s/CONFIG=.*/CONFIG=mainnet_pruned/" roles/nethermind-docker-compose/files/common/.env.common
     fi
 fi
-
-while true; do
-    read -e -p "Do you wish to install New Relic Agent (License Key required)? (Y/n) " yn
-    case $yn in
-        [Yy]* ) read -e -s -p "Provide New Relic license key: " LICENSE_KEY
-                ansible-playbook -i "$PUBLIC_IP," --private-key $KEY_PATH --extra-vars "ansible_user=nethermind license_key=$LICENSE_KEY" playbooks/setup-newrelic.yml;
-                break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
